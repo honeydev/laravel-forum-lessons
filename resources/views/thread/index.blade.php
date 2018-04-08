631022d5c1cda2ca737de9622ec079c360c76e87
honeydev <lebedev.alexey@protonmail.com>
2018-04-09 00:24:53 +1000

    Update index.blade.php

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -5,8 +5,6 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
                     @foreach($threads as $thread)
                         <h4>{{$thread->title}}</h4>
